a7c701d887e42de8bd677d7b631918b96f1b324a
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2022-03-23 12:14:05 -0400

    Update TicketHistroy Service Class and Startup.cs
    Updated the GetProjectTicketsHistoriesAsync method to be asynchronous.
    Try to acess project table where our companies id matches the one we pass in.
    Include tickets, ticket histories, user, then get the first or default were project id matches the Id we pass in.
    Return a list of ticket history to a list for each project we pass in.
    Return the ticket history or if failed catch and throw an exception.
    
    Registered the ticket history interface and implementing class for the service in the startup.cs in the configure services method.

--- TheBugTracker/Services/BTTicketHistoryService.cs
+++ TheBugTracker/Services/BTTicketHistoryService.cs
@@ -173,9 +173,25 @@ namespace TheBugTracker.Services
             }
         }
 
-        public Task<List<TicketHistory>> GetProjectTicketsHistoriesAsync(int projectId, int companyId)
+        public async Task<List<TicketHistory>> GetProjectTicketsHistoriesAsync(int projectId, int companyId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Project project = await _context.Projects.Where(p => p.CompanyId == companyId)
+                                                         .Include(p => p.Tickets)
+                                                            .ThenInclude(t => t.History)
+                                                                .ThenInclude(h => h.User)
+                                                         .FirstOrDefaultAsync(p => p.Id == projectId);
+
+                List<TicketHistory> ticketHistories = project.Tickets.SelectMany(t => t.History).ToList();
+
+                return ticketHistories;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
