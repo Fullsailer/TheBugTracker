b402c635a47f1f408ceba20496017173f1862fe1
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2022-03-30 11:39:29 -0400

    Create IBTNotificationService.cs
    Added new IBTNotificatoinService interface.
    Make it public,
    Add methods and define methods for the interface: Add Notification, Get Received Notifications, Get Sent Notifications, Send Email Notifications By Role, Send Member Email Notification, Send Email Notification. Make all of these methods Asynchronous.

---
+++ TheBugTracker/Services/Interfaces/IBTNotificationService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBugTracker.Models;
+
+namespace TheBugTracker.Services.Interfaces
+{
+    public interface IBTNotificationService
+    {
+        public Task AddNotificationAsync(Notification notification);
+
+        public Task<List<Notification>> GetReceivedNotificationAsync(string userId);
+
+        public Task<List<Notification>> GetSentNotificationAsync(string userId);
+
+        public Task SendEmailNotificationsByRoleAsync(Notification notification, int companyId, string role);
+
+        public Task SendMembersEmailNotificationAsync(Notification notification, List<BTUser> members);
+
+        public Task SendEmailNotificationAsync(Notification notification, string emailSubject);
+    }
+}
