403e39932ad11870c15316653ba4ef29f4adda63
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2022-03-30 11:52:00 -0400

    Create BTNotification.cs
    Added New Class BTNotification to services using the IBTNotificationService.
    Implemented methods from the IBTNotificationService in the BTNotifcation service.
    Added injections need for this service to run: ApplicationDBContext, IEmailSender, and IBTRolesService.
    Updated AddNotificationAsync to be async and to try to contact the db go to the notification table  and then save changes. If error occurs then throw exception.

---
+++ TheBugTracker/Services/BTNotification.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBugTracker.Data;
+using TheBugTracker.Models;
+using TheBugTracker.Services.Interfaces;
+
+namespace TheBugTracker.Services
+{
+    public class BTNotification : IBTNotificationService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IEmailSender _emailSender;
+        private readonly IBTRolesService _rolesService;
+
+        public BTNotification(ApplicationDbContext context,
+                              IEmailSender emailSender,
+                              IBTRolesService rolesService)
+        {
+            _context = context;
+            _emailSender = emailSender;
+            _rolesService = rolesService;
+        }
+        public async Task AddNotificationAsync(Notification notification)
+        {
+            try
+            {
+                await _context.AddAsync(notification);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Task<List<Notification>> GetReceivedNotificationAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Notification>> GetSentNotificationAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendEmailNotificationAsync(Notification notification, string emailSubject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendEmailNotificationsByRoleAsync(Notification notification, int companyId, string role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendMembersEmailNotificationAsync(Notification notification, List<BTUser> members)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
