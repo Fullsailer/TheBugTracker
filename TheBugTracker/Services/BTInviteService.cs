55f99224b17985b540fe9e127bae661bb11081e1
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2022-03-31 13:59:13 -0400

    Update BTInviteService.cs
    Added directive for using the IBTInviteService interface and implemented the methods from that interface.

--- TheBugTracker/Services/BTInviteService.cs
+++ TheBugTracker/Services/BTInviteService.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheBugTracker.Models;
+using TheBugTracker.Services.Interfaces;
 
 namespace TheBugTracker.Services
 {
-    public class BTInviteService
+    public class BTInviteService : IBTInviteService
     {
+        public Task<bool> AcceptInviteAsync(Guid? token, string userId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddNewInviteAsync(Invite invite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AnyInviteAsync(Guid token, string email, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Invite> GetInviteAsync(int inviteId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Invite> GetInviteAsync(Guid token, string email, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ValidateInviteCodeAsync(Guid? token)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
