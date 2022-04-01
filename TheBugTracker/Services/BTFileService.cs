7779a024d53f6d924908245b2e8d5b6ca821597a
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2022-04-01 12:01:50 -0400

    Update BTFileService.cs
    Updated ConvertFileToByteArrayAsync to be async.
    Try to create an instance of a class MemoryStream from system.IO await the Iformfile we are passing in to the targe stream memoryStream then turn that memoryStream in  byte array named bytefile.
    Then close memoryStream and dispose of memory in memoryStream.
    The return bytefile if failed then throw and exception.

--- TheBugTracker/Services/BTFileService.cs
+++ TheBugTracker/Services/BTFileService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using TheBugTracker.Services.Interfaces;
@@ -24,9 +25,23 @@ namespace TheBugTracker.Services
             }
         }
 
-        publicb aync Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
+        public async Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
         {
-            throw new NotImplementedException();
+            try
+            {
+                MemoryStream memoryStream = new();
+                await file.CopyToAsync(memoryStream);
+                byte[] byteFile = memoryStream.ToArray();
+                memoryStream.Close();
+                memoryStream.Dispose();
+
+                return byteFile;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public string FormatFilesSize(long bytes)
