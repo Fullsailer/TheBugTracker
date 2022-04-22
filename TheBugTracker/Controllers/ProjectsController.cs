81114989bb4a57013fe5a11f92a2c74ce43d9f7d
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2022-04-22 15:15:15 -0400

    Update ProjectsController.cs
    Added Using Directive to the controller for the model enums.
    Add if statement to see if the user is in role of adim and remove the My Projects drop down.

--- TheBugTracker/Controllers/ProjectsController.cs
+++ TheBugTracker/Controllers/ProjectsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -16,32 +17,75 @@ namespace TheBugTracker.Controllers
 {
     public class ProjectsController : Controller
     {
+        #region Properties
         private readonly ApplicationDbContext _context;
         private readonly IBTRolesService _rolesService;
         private readonly IBTLookupService _lookupService;
         private readonly IBTFileService _fileService;
         private readonly IBTProjectService _projectService;
+        private readonly IBTCompanyInfoService _companyInfoService;
+        private readonly UserManager<BTUser> _userManager;
+        #endregion
 
+        #region Constructor
         public ProjectsController(ApplicationDbContext context,
                                     IBTRolesService rolesService,
                                     IBTLookupService lookupService,
-                                    IBTFileService fileService, 
-                                    IBTProjectService projectService)
+                                    IBTFileService fileService,
+                                    IBTProjectService projectService,
+                                    IBTCompanyInfoService companyInfoService,
+                                    UserManager<BTUser> userManager)
         {
             _context = context;
             _rolesService = rolesService;
             _lookupService = lookupService;
             _fileService = fileService;
             _projectService = projectService;
+            _companyInfoService = companyInfoService;
+            _userManager = userManager;
         }
+        #endregion
 
+        #region Index
         // GET: Projects
         public async Task<IActionResult> Index()
         {
             var applicationDbContext = _context.Projects.Include(p => p.Company);
             return View(await applicationDbContext.ToListAsync());
         }
+        #endregion
 
+        #region My Projects
+        public async Task<IActionResult> MyProjects()
+        {
+            string userId = _userManager.GetUserId(User);
+
+            List<Project> projects = await _projectService.GetUserProjectsAsync(userId);
+
+            return View(projects);
+        }
+        #endregion
+
+        #region All Projects
+        public async Task<IActionResult> AllProjects()
+        {
+            List<Project> projects = new();
+
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            if (User.IsInRole(nameof(Roles.Admin)) || User.IsInRole(nameof(Roles.ProjectManager)))
+            {
+                projects = await _companyInfoService.GetAllProjectsAsync(companyId);
+            }
+            else
+            {
+                projects = await _projectService.GetAllProjectsByCompanyAsync(companyId);
+            }
+            return View(projects);
+        }
+        #endregion
+
+        #region Projects Details
         // GET: Projects/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -60,7 +104,9 @@ namespace TheBugTracker.Controllers
 
             return View(project);
         }
+        #endregion
 
+        #region Get Projects Create
         // GET: Projects/Create
         public async Task<IActionResult> Create()
         {
@@ -70,12 +116,14 @@ namespace TheBugTracker.Controllers
             AddProjectWithPMViewModel model = new();
 
             //Load SelectLists with data ie. PMList & PriorityList
-            model.PMList = new SelectList(await _rolesService.GetUserInRoleAsync(Roles.ProjectManager.ToString(),companyId),"Id","FullName");
-            model.PriorityList = new SelectList(await _lookupService.GetProjectPrioritiesAsync(),"Id", "Name");
+            model.PMList = new SelectList(await _rolesService.GetUserInRoleAsync(Roles.ProjectManager.ToString(), companyId), "Id", "FullName");
+            model.PriorityList = new SelectList(await _lookupService.GetProjectPrioritiesAsync(), "Id", "Name");
 
             return View(model);
         }
+        #endregion
 
+        #region Post Projects Create
         // POST: Projects/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -83,16 +131,16 @@ namespace TheBugTracker.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(AddProjectWithPMViewModel model)
         {
-            if (model !=null)
+            if (model != null)
             {
                 int companyId = User.Identity.GetCompanyId().Value;
                 try
                 {
-                    if(model.Project.ImageFormFile != null)
+                    if (model.Project.ImageFormFile != null)
                     {
                         model.Project.ImageFileData = await _fileService.ConvertFileToByteArrayAsync(model.Project.ImageFormFile);
                         model.Project.ImageFileName = model.Project.ImageFormFile.FileName;
-                        model.Project.ImageFileContentType = model.Project.ImageFormFile.ContentType; 
+                        model.Project.ImageFileContentType = model.Project.ImageFormFile.ContentType;
                     }
                     model.Project.CompanyId = companyId;
 
@@ -115,7 +163,9 @@ namespace TheBugTracker.Controllers
             }
             return RedirectToAction("Create");
         }
+        #endregion
 
+        #region Get Projects Edit
         // GET: Projects/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -132,7 +182,9 @@ namespace TheBugTracker.Controllers
 
             return View(model);
         }
+        #endregion
 
+        #region Post Projects Edit
         // POST: Projects/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -167,12 +219,14 @@ namespace TheBugTracker.Controllers
 
                     throw;
                 }
-                
-                
+
+
             }
             return RedirectToAction("Edit");
         }
+        #endregion
 
+        #region Get Projects Archive
         // GET: Projects/Archive/5
         public async Task<IActionResult> Archive(int? id)
         {
@@ -192,7 +246,9 @@ namespace TheBugTracker.Controllers
 
             return View(project);
         }
+        #endregion
 
+        #region Post Projects Archive
         // POST: Projects/Archive/5
         [HttpPost, ActionName("Archive")]
         [ValidateAntiForgeryToken]
@@ -206,7 +262,9 @@ namespace TheBugTracker.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+        #endregion
 
+        #region Get Projects Restore
         // GET: Projects/Restore/5
         public async Task<IActionResult> Restore(int? id)
         {
@@ -226,7 +284,9 @@ namespace TheBugTracker.Controllers
 
             return View(project);
         }
+        #endregion
 
+        #region Post Projects Restore
         // POST: Projects/Restore/5
         [HttpPost, ActionName("Restore")]
         [ValidateAntiForgeryToken]
@@ -240,11 +300,15 @@ namespace TheBugTracker.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+        #endregion
 
 
+        #region Project Exists
         private bool ProjectExists(int id)
         {
             return _context.Projects.Any(e => e.Id == id);
         }
+
+        #endregion
     }
 }
