ab17708d3dee1e3dda2c72dcbbe245e7216fcd7b
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2022-05-05 17:30:27 -0400

    Update BTProjectService.cs
    Added GetArchivedProjectsByCompanyAsync method to use the company ID to get a list of projects and include the following data from the database and return it to our projects variable.

--- TheBugTracker/Services/BTProjectService.cs
+++ TheBugTracker/Services/BTProjectService.cs
@@ -179,11 +179,40 @@ namespace TheBugTracker.Services
         #endregion
 
         #region Get Archived Projects By Company Id
-        public async Task<List<Project>> GetArchivedProjectsByCompany(int companyId)
+        public async Task<List<Project>> GetArchivedProjectsByCompanyAsync(int companyId)
         {
-            List<Project> projects = await GetAllProjectsByCompanyAsync(companyId);
+            try
+            {
+                List<Project> projects = await _context.Projects.Where(p => p.CompanyId == companyId && p.Archived == true)
+                                                   .Include(p => p.Members)
+                                                   .Include(p => p.Tickets)
+                                                       .ThenInclude(t => t.Comments)
+                                                   .Include(p => p.Tickets)
+                                                       .ThenInclude(t => t.Attachments)
+                                                   .Include(p => p.Tickets)
+                                                       .ThenInclude(t => t.History)
+                                                   .Include(p => p.Tickets)
+                                                       .ThenInclude(t => t.Notifications)
+                                                   .Include(p => p.Tickets)
+                                                       .ThenInclude(t => t.DeveloperUser)
+                                                   .Include(p => p.Tickets)
+                                                       .ThenInclude(t => t.OwnerUser)
+                                                   .Include(p => p.Tickets)
+                                                       .ThenInclude(t => t.TicketStatus)
+                                                   .Include(p => p.Tickets)
+                                                       .ThenInclude(t => t.TicketPriority)
+                                                   .Include(p => p.Tickets)
+                                                       .ThenInclude(t => t.TicketType)
+                                                   .Include(p => p.ProjectPriority)
+                                                   .ToListAsync();
 
-            return projects.Where(p => p.Archived == true).ToList();
+                return projects;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         #endregion
