074de0c77c399f4efca25c7fac81fe52a9a4e73c
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2022-05-15 21:03:58 -0400

    Update TicketsController.cs
    Added Regions to clean up code.
    Added my Ticket method to show a users tickets in the my ticket view.
    Updated the Delete methods to archive methods using the ticket service to retrieve the ticket data rather than the db context service.
    Added Restore ticket methods that are built using the archive formerly delete methods with some new code in the restore post method that  changes the archived status for false instead of true and updates the ticket.

--- TheBugTracker/Controllers/TicketsController.cs
+++ TheBugTracker/Controllers/TicketsController.cs
@@ -39,14 +39,27 @@ namespace TheBugTracker.Controllers
         }
         #endregion
 
-        #region Get Index
+        #region Get Tickets
         // GET: Tickets
         public async Task<IActionResult> Index()
         {
             var applicationDbContext = _context.Tickets.Include(t => t.DeveloperUser).Include(t => t.OwnerUser).Include(t => t.Project).Include(t => t.TicketPriority).Include(t => t.TicketStatus).Include(t => t.TicketType);
             return View(await applicationDbContext.ToListAsync());
         }
+        #endregion
 
+        #region Get My Tickets
+        public async Task<IActionResult> MyTickets()
+        {
+            BTUser btUser = await _userManager.GetUserAsync(User);
+
+            List<Ticket> tickets = await _ticketService.GetTicketsByUserIdAsync(btUser.Id, btUser.CompanyId);
+
+            return View(tickets);
+        } 
+        #endregion
+
+        #region Get Ticket Details
         // GET: Tickets/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -69,7 +82,7 @@ namespace TheBugTracker.Controllers
             }
 
             return View(ticket);
-        }
+        } 
         #endregion
 
         #region Get Create
@@ -208,23 +221,17 @@ namespace TheBugTracker.Controllers
         }
         #endregion
 
-        #region Get Delete
-        // GET: Tickets/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        #region Get Archive
+        // GET: Tickets/Archive/5
+        public async Task<IActionResult> Archive(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var ticket = await _context.Tickets
-                .Include(t => t.DeveloperUser)
-                .Include(t => t.OwnerUser)
-                .Include(t => t.Project)
-                .Include(t => t.TicketPriority)
-                .Include(t => t.TicketStatus)
-                .Include(t => t.TicketType)
-                .FirstOrDefaultAsync(m => m.Id == id);
+            Ticket ticket = await _ticketService.GetTicketByIdAsync(id.Value);
+
             if (ticket == null)
             {
                 return NotFound();
@@ -234,15 +241,50 @@ namespace TheBugTracker.Controllers
         }
         #endregion
 
-        #region Post Delete
-        // POST: Tickets/Delete/5
-        [HttpPost, ActionName("Delete")]
+        #region Post Archive
+        // POST: Tickets/Archive/5
+        [HttpPost, ActionName("Archive")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> ArchiveConfirmed(int id)
         {
-            var ticket = await _context.Tickets.FindAsync(id);
-            _context.Tickets.Remove(ticket);
-            await _context.SaveChangesAsync();
+            Ticket ticket = await _ticketService.GetTicketByIdAsync(id);
+            ticket.Archived = true;
+            await _ticketService.UpdateTicketAsync(ticket);
+
+            return RedirectToAction(nameof(Index));
+        }
+        #endregion
+
+        #region Get Archive Restore
+        // GET: Tickets/Restore/5
+        public async Task<IActionResult> Restore(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Ticket ticket = await _ticketService.GetTicketByIdAsync(id.Value);
+
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticket);
+        }
+        #endregion
+
+        #region Post Archive Restore
+        // POST: Tickets/Restore/5
+        [HttpPost, ActionName("Restore")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RestoreConfirmed(int id)
+        {
+            Ticket ticket = await _ticketService.GetTicketByIdAsync(id);
+            ticket.Archived = false;
+            await _ticketService.UpdateTicketAsync(ticket);
+
             return RedirectToAction(nameof(Index));
         }
         #endregion
