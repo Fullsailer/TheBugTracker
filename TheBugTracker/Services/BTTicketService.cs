6868f01b34b3514c56756e49d213de736998103d
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2022-06-14 12:07:40 -0400

    Update BTTicketService.cs
    Added GetUnassignedTicketsAsync method tot eh ticket service to get a list of all ticket by company where the ticket Deverloper User Id is null or empty.

--- TheBugTracker/Services/BTTicketService.cs
+++ TheBugTracker/Services/BTTicketService.cs
@@ -467,6 +467,25 @@ namespace TheBugTracker.Services
         }
         #endregion
 
+        #region Get Unassigned Tickets
+        public async Task<List<Ticket>> GetUnassignedTicketsAsync(int companyId)
+        {
+            List<Ticket> tickets = new();
+
+            try
+            {
+                tickets = (await GetAllTicketsByCompanyAsync(companyId)).Where(t=> string.IsNullOrEmpty(t.DeveloperUserId)).ToList();
+                return tickets;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+
+
         #region Lookup Ticket Priority Id
         public async Task<int?> LookupTicketPriorityIdAsync(string priorityName)
         {
